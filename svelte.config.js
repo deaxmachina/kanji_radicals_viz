a0760187eb51a704ee65dbb6d0fdef69ded40300
Dea Bankova <d.y.bankova@gmail.com>
2022-06-03 18:32:47 +0100

    modified svelte config for deploymnet

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,37 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import sveltePreprocess from 'svelte-preprocess'
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: sveltePreprocess(),
 	kit: {
-		adapter: adapter()
+		paths: {
+			assets:
+			  process.env.NODE_ENV === 'production' ? 'https://deaxmachina.github.io/kanji_radicals_viz/cdn' : '',
+			base:
+			  process.env.NODE_ENV === 'production'
+				? '/kanji_radicals_viz'
+				: '',
+		},
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs/cdn',
+			fallback: null,
+		  }),
+		trailingSlash: 'always',
+		prerender: {
+			default: true
+		  },
+		vite: {
+			ssr: {
+				noExternal: [],
+			},
+			optimizeDeps: {
+				exclude: [],
+				include: [],
+			  },
+		}
 	}
 };
 
