c5ae81c7158433ae85650a84c0f313acda6d7803
Dea Bankova <d.y.bankova@gmail.com>
2022-06-05 21:41:24 +0100

    graph styling and layout

--- src/lib/components/KanjiGraph/KanjiGraph.js
+++ src/lib/components/KanjiGraph/KanjiGraph.js
@@ -32,7 +32,7 @@ class KanjiGraph {
 	
 	draw() {
     const { nodes, links, nodesCopy, linksCopy, subcategories, dataKanjiLevels } = this.data();
-    const { width, height, aspectRatioHorizontal, colours, graphProps, grades, mobile } = this.props();
+    const { width, height, aspectRatioHorizontal, colours, graphProps, grades, mobile, selectedCategory } = this.props();
 
     let simulation
 
@@ -51,8 +51,37 @@ class KanjiGraph {
       .attr('transform', `translate(${ 
         width*0.5
       }, ${
-        height*0.1
+        aspectRatioHorizontal ? height*0.17 : height*0.1
       })`)
+
+    ////////////////////////////////
+    /////////// Filters ////////////
+    ////////////////////////////////
+    const defs = svg.appendSelect("defs")
+    defs.appendSelect("filter")
+    .attr("id", "noise")
+    .appendSelect("feTurbulence")
+      .attr('type', 'fractalNoise')
+      .attr('baseFrequency', '1.29')
+      .attr('numOctaves', '2')
+      .attr('result', 'noisy')
+    .appendSelect("feColorMatrix")
+      .attr('type', 'saturate')
+      .attr('values', 0)
+    .appendSelect("feBlend")
+      .attr('in', 'SourceGraphic')
+      .attr('in2', 'noisy')
+      .attr('mode', 'multiply')
+
+      // const bg = g.appendSelect('rect.bg-rect')
+      // .attr('x', -width/2)
+      // .attr('y', -height*0.1)
+      // .attr('width', width)
+      // .attr('height', height)
+      // .style('fill', colours.colBg)
+      // .style("filter", "url(#noise)")
+      // .style('opacity', 0.3)
+
 
     ////////////////////////////////
     /////////// Scales /////////////
@@ -60,7 +89,7 @@ class KanjiGraph {
     // Scale for desktop to position in a circle
     const subcategoryRadialScale = d3.scaleBand()
       .domain(subcategories)
-      .range([Math.PI*3/2, Math.PI*0.5])
+      .range([Math.PI*1.5, Math.PI*0.45])
     // Scale for mobile to position vertically 
     const subcategoryVerticalScale = d3.scaleBand()
       .domain(subcategories)
@@ -77,11 +106,11 @@ class KanjiGraph {
     const scaleFunctionNodes = d => `scale(${scaleFunction(d)})`
     //const radiusCollide = d => graphProps.widthKanjiBox * 0.6 * scaleFunction(d)
     const radiusCollide = d => d.type === 'radical' 
-      ? graphProps.widthKanjiBox*0.4 + graphProps.widthKanjiBox * (linksCopy.filter(l => l.source === d.id).length * graphProps.scaleFactorDeg) 
-      : graphProps.widthKanjiBox*0.6
+      ? graphProps.widthKanjiBox*0.45 + graphProps.widthKanjiBox * 0.9 * (linksCopy.filter(l => l.source === d.id).length * graphProps.scaleFactorDeg) 
+      : graphProps.widthKanjiBox*0.66
 
     // Forces 
-    const linkForce = d3.forceLink(links).id(d => d.id)
+    const linkForce = d3.forceLink(links).id(d => d.id).strength(selectedCategory === 'Physical Attributes and Properties' ? 0.25 : 0.35)
     const collideForce = d3.forceCollide().radius(radiusCollide).iterations(2).strength(1)
     // Radically position around a circle
     const xForceRadial = d3.forceX(d => (d.type === 'kanji' && d.subcategory !== 0)
@@ -91,13 +120,13 @@ class KanjiGraph {
     const yForceRadial = d3.forceY(d => (d.type === 'kanji' && d.subcategory !== 0)
       ? d3.pointRadial(subcategoryRadialScale(d.subcategory), graphProps.radiusGroups)[1]
       : 0
-    ).strength(1)
+    ).strength(1.3)
     // Vertically position 
     const yForce = d3.forceY(d => (d.type === 'kanji' && d.subcategory !== 0)
       ? subcategoryVerticalScale(d.subcategory)
-      : 0
-    ).strength(3)
-    const xForce = d3.forceX(d => 0).strength(1)
+      : 40
+    ).strength(5)
+    const xForce = d3.forceX(d => 0).strength(2)
 
     // Simulation with forces 
     if (aspectRatioHorizontal) {
@@ -130,12 +159,13 @@ class KanjiGraph {
     ////////////////////////////////
     /////////// Links //////////////
     ////////////////////////////////
-    const link = g.appendSelect("g")
+    const linkG = g.appendSelect("g.g-links")
       .attr('class', 'g-links')
       .style("stroke", colours.colLinks)
-      .style("stroke-opacity", 0.2)
-      .style("stroke-width", 0.3)
-      .selectAll(".link")
+      .style("stroke-opacity", 0.5)
+      .style("stroke-width", 0.4)
+      
+    const linksLine = linkG.selectAll(".link")
         .data(links)
         .join("line")
         .classed('link', true)
@@ -147,7 +177,7 @@ class KanjiGraph {
     ////////////////////////////////
     /////////// Nodes //////////////
     ////////////////////////////////
-    const nodeG = g.appendSelect("g")
+    const nodeG = g.appendSelect("g.g-nodes")
       .attr('class', 'g-nodes')
 
     // One group for each node to include node box and kanji 
@@ -168,15 +198,22 @@ class KanjiGraph {
         .attr('width', graphProps.widthKanjiBox)
         .attr('height', graphProps.widthKanjiBox)
         .attr('transform', d => scaleFunctionNodes(d))
-        .style('stroke', d => d.type === 'kanji' ? colours.colStrokeKanji : colours.colStrokeRadicals)
-        .style('stroke-width', 1)
-        .style("stroke-opacity", 1)
+        .style('stroke', d => d.type === 'kanji' 
+          //? colours.colStrokeKanji 
+            ? _.find(dataKanjiLevels, e => e.kanji === d.kanji)
+            ? colourByGradeScale( _.find(dataKanjiLevels, e => e.kanji === d.kanji).Grade )
+            : colours.colMissingKanji
+          : colours.colStrokeRadicals
+        )
+        .style('stroke-width', d => d.type === 'kanji' ? 4 : 2)
+        .style("stroke-opacity", d => d.type === 'kanji' ? 0.9 : 1)
         .style("fill", d => d.type === 'kanji' 
               ? _.find(dataKanjiLevels, e => e.kanji === d.kanji)
                 ? colourByGradeScale( _.find(dataKanjiLevels, e => e.kanji === d.kanji).Grade )
                 : colours.colMissingKanji
               : colours.colRadicals
               )
+        .style('rx', d => d.type === 'kanji' ? 0 : 10)
 
     const nodeText = nodesG.selectAll(".node-text")
       .data(d => [d])
@@ -187,7 +224,7 @@ class KanjiGraph {
         .text(d => d.kanji)
         .attr('text-anchor', 'middle')
         .attr('dy', '0.35em')
-        .style('font-size', `${graphProps.widthKanjiBox * 0.8}px`)
+        .style('font-size', `${graphProps.widthKanjiBox * 0.75}px`)
         //.style('font-weight', 'lighter')
         .style('stroke', 'none')
         .style('fill', d => d.type === 'radical' ? colours.colRadicalsText : colours.colKanjiText)
--- src/lib/components/KanjiGraph/KanjiGraph.svelte
+++ src/lib/components/KanjiGraph/KanjiGraph.svelte
@@ -19,28 +19,32 @@
   // Properties based on the width and height of the container
   $: aspectRatio = w / h
   $: aspectRatioHorizontal = aspectRatio > 1
-  const marginForOuterCircle = 200 // How much to leave around outer circle of kanji so that they comfortably fit
+  // How much to leave around outer circle of kanji so that they comfortably fit
+  $: marginForOuterCircle = w >= 1300 ? 200 : w >= 1200 ? 300 : aspectRatioHorizontal ? 400 : 50
   $: radiusGroups = Math.min(w, h) - marginForOuterCircle // Radius for the kanji around the radicals
-  $: widthKanjiBox = w >= 800 ? 24 : 20 // Size of the kanji squares (or circles) depending on width
+  $: widthKanjiBox = w >= 800 ? 26 : 20 // Size of the kanji squares (or circles) depending on width
   // Things that depend on the filtered data
   $: links = dataGraphFiltered.links.map(d => Object.create(d));
   $: nodes = dataGraphFiltered.nodes.map(d => Object.create(d));
   $: subcategories = _.uniqBy(nodes, d => d.subcategory).map(d => d.subcategory).filter(d => d !== 0)
+  $: console.log('subcategories', subcategories)
   // Constant props 
   const grades = ['1', '2', '3', '4', '5', '6', 'S']
   const colours = {
-      colBg: '#352d39',
-      colLinks: "#b1a7a6",      
-      colRadicals: '#f7ede2', //'#5e6472',
-      colRadicalsText: '#352d39',
-      colStrokeRadicals: "#fff",  
+      colBg: '#f7ede2',//'#f7ede2', //'#352d39',
+      colLinks: "#fff",      
+      colRadicals: '#fff' ,//'#f7ede2', 
+      colRadicalsText: '#251926',
+      colStrokeRadicals: "#251926",  
       colKanjiText: '#fff',
-      colStrokeKanji: "#111",
-      colLevels: ['#cce3de', '#a4c3b2', '#6b9080', '#d1b3c4', '#b392ac', '#735d78', '#fe6d73'],
-      colMissingKanji: '#111111',
+      colStrokeKanji: "#251926",
+      //colLevels: ['#cce3de', '#a4c3b2', '#6b9080', '#d1b3c4', '#b392ac', '#735d78', '#fe6d73'],
+      //colLevels: ['#b9c1cd', '#cfa1cb', '#ce8cbd', '#c170ab', '#a15da2', '#844588', '#8dcaab'],
+      colLevels: ['#8dcaab', '#9fcfc0', '#bdb7cc', '#cfa1cb', '#ce8cbd', '#c170ab', '#a15da2'],
+      colMissingKanji: '#4d5054',
       
     }
-  const scaleFactorDeg = 0.06 // Scale factor for the degree of the nodes of the radicals
+  const scaleFactorDeg = 0.04 // Scale factor for the degree of the nodes of the radicals
 
   // The graph props are reactive as they depend on other reactive declarations or variable
 	$: props = {
@@ -54,7 +58,8 @@
       scaleFactorDeg
     },
     colours,
-    grades
+    grades,
+    selectedCategory
 	}
 
   ////////////////////////////
@@ -110,9 +115,10 @@
 
 
 <style lang='scss'>
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap');
   #kanji-graph-section {
     width: 100vw;
-    background-color: red;
+    //background-color: red;
   }
   #kanji-graph-container {
     background-color: red;
@@ -124,12 +130,13 @@
         text.node-text {
         }
         text.radical-text {
-          font-weight: 900;
-          font-family:'Times New Roman', Times, serif;
+          font-weight: 300;
+          //font-family:'Times New Roman', Times, serif;
+          font-family: 'Noto Sans JP', sans-serif;
         }
         text.kanji-text {
-          font-weight: 100;
-          font-family: Arial, Helvetica, sans-serif;
+          font-weight: 300;
+          font-family: 'Noto Sans JP', sans-serif;
         }
   }
 </style>
